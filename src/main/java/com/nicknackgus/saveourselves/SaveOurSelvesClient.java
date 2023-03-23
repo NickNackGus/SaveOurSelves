a53e135b4454303f35fdf9b79308d9586677dabc
Timothy Southwick <NickNackGus@gmail.com>
2023-03-22 20:04:09 -0400

    Added map color export (not for release)

--- src/main/java/com/nicknackgus/saveourselves/SaveOurSelvesClient.java
+++ src/main/java/com/nicknackgus/saveourselves/SaveOurSelvesClient.java
@@ -5,11 +5,14 @@ import com.google.gson.JsonParseException;
 import com.nicknackgus.saveourselves.options.Options;
 import com.nicknackgus.saveourselves.alarms.HeartbeatAlarm;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.block.MapColor;
 import net.minecraft.client.MinecraftClient;
 
 @Environment(EnvType.CLIENT)
@@ -38,6 +41,39 @@ public class SaveOurSelvesClient implements ClientModInitializer {
 		}
 
 		ClientTickEvents.END_CLIENT_TICK.register(new HeartbeatAlarm());
+
+		try (FileWriter writer = new FileWriter((FabricLoader.getInstance().getConfigDir().resolve("exported_map_colors.csv").toFile()))) {
+			writer.write("Render Color Byte\tMap Color\tBrightness\tAlpha\tRed\tGreen\tBlue\n");
+			for (int i = 0; i < 65536; i++) {
+				MapColor mapColor;
+				try {
+					mapColor = MapColor.get(i);
+				} catch (IndexOutOfBoundsException unused) {
+					break;
+				}
+				if (i != 0 && MapColor.CLEAR.equals(mapColor)) {
+					break;
+				}
+				for (MapColor.Brightness brightness : MapColor.Brightness.values()) {
+					int renderColor = mapColor.getRenderColor(brightness);
+					int renderColorByte = (mapColor.id << 2) | (brightness.id & 0x3);
+					int a = (renderColor >> 24) & 0xff;
+					int r = (renderColor >> 16) & 0xff;
+					int g = (renderColor >> 8) & 0xff;
+					int b = renderColor & 0xff;
+
+					writer.write("" + renderColorByte +
+							'\t' + mapColor.id +
+							'\t' + brightness.id +
+							'\t' + a +
+							'\t' + r +
+							'\t' + g +
+							'\t' + b + '\n');
+				}
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	public static void saveConfig() {
